9c5c1b7fb8e66f1af98b7199679c3697e7f3c435
sramosbernal22 <sramosbernal22@gmail.com>
2024-08-19 16:07:41 -0500

    Created basic format in javascript to load functins after DOM Loads Also made basic HTML page with javascript attached and some buttons for visuals.

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function add(numberOne, numberTwo){
+
+    };
+
+    function subtract(numberOne, numberTwo){
+
+    };
+
+    function add(numberOne, numberTwo){
+
+    };
+
+    function multiply(numberOne, numberTwo){
+
+    };
+
+    function divide(numberOne, numberTwo){
+
+    };
+
+
+
+
+  });
